80fddc4e219a98b49faf1dfcfced4d1eb0e8bb38
Abhilash A K <abhilashamballur@gmail.com>
2019-06-05 01:14:43 +0530

    Code optimizations and refactoring

--- lib/ph_no_to_word.rb
+++ lib/ph_no_to_word.rb
@@ -17,7 +17,7 @@
 # * cat, boot, our
 require 'ph_no_to_word/version'
 require 'ph_no_to_word/constants'
-require 'ph_no_to_word/error'
+require 'ph_no_to_word/validator'
 require 'ph_no_to_word/logger'
 require 'ph_no_to_word/helpers'
 require 'ph_no_to_word/split_dictionary'
@@ -29,80 +29,84 @@ module PhNoToWord
   include Constants
   include Helpers
   include SplitDictionary
-  include Error
+  include Validator
   extend Logger
 
   def self.extended(base)
     base.send(:include, Constants)
   end
 
-  # @param phone_no [String]
+  # @param ph_number [String]
   # @return words [Array]
-  # Ex: PhNoToWord::convert "2282668687"
+  # Ex: PhNoToWord::convert '2282668687'
   # And add `split_files(nil)` in line 1 for splitting the dictionary file
-  def self.convert(phone_no = '')
-    ph_numbers = phone_no.split(//)
-    validate phone_no, ph_numbers
+  def self.convert(ph_number = '')
+    ph_ary = ph_number.split(//)
+    validate ph_number, ph_ary
 
-    ph_to_word_mapping = ph_numbers.map { |ph_no| NO_CHAR_MAP[ph_no.to_sym] }
+    ph_to_word_mapping = ph_ary.map { |ph_no| NO_CHAR_MAP[ph_no.to_sym] }
+
     matching_wd_hash = search_for_wds(ph_to_word_mapping)
+
     reset_data_stores
-    map_num_to_word_hash(matching_wd_hash, ph_numbers)
-  end
-
-  # @param phone_no [String] ph_numbers [Array]
-  # Populate error if not a valid phone number given
-  # Ex: ph_numbers ["2", "2", "8", "2", "6", "6", "8", "6", "8", "7"]
-  def self.validate(phone_no, ph_numbers)
-    raise RequiredArgumentMissingError, ERRORS[:missing_ph] if phone_no.empty?
-
-    unless phone_no.length.eql?(PH_LENGTH)
-      raise MalformattedArgumentError, ERRORS[:ph_length]
-    end
-    return if (ph_numbers & FORBIDDEN_NOS).empty?
-
-    raise MalformattedArgumentError, ERRORS[:malformed_ph_no]
+    map_num_to_word_hash(matching_wd_hash, ph_ary)
   end
 
   # Combaine the first second and third words to form the result
-  # @param wd_hash [Hash] ph_numbers [Array]
-  # Ex: ph_numbers ["2", "2", "8", "2", "6", "6", "8", "6", "8", "7"]
+  # @param wd_hash [Hash] ph_ary [Array]
+  # Ex: ph_ary ["2", "2", "8", "2", "6", "6", "8", "6", "8", "7"]
   # Ex: wd_hash { :first => "act", :second => "amounts", :third => nil}
-  def self.map_num_to_word_hash(wd_hash = {}, ph_numbers)
-    full_match = []
-    ten_char_seached_files = []
+  def self.map_num_to_word_hash(wd_hash = {}, ph_ary)
+    matches = []
+    chkd_ten_chr_fls = []
 
     wd_hash[:first].each do |first_wd|
-      wd_hash[:second].each do |second_wd|
-        first_two_wds = first_wd + second_wd
+      matches = match_with_nxt_wd(:second, wd_hash, ph_ary, matches, first_wd)
 
-        if first_two_wds.length == 10 && wds_satisfy_pos?(0, first_two_wds, ph_numbers)
-          full_match << "#{first_wd}, #{second_wd}".downcase
-        end
+      matches, chkd_ten_chr_fls = find_ten_chr_wds(first_wd,
+                                                   ph_ary,
+                                                   chkd_ten_chr_fls,
+                                                   matches)
+    end
+    matches
+  end
 
-        if first_two_wds.length <= 7
-          wd_hash[:third].each do |trd_wd|
-            three_wd = first_two_wds + trd_wd
+  # combaine the word with next word and check matches the ph number
+  # If the word is second matched word and less than max wd length
+  # then match it with third word
+  def self.match_with_nxt_wd(pos, wd_hash, ph_ary, matches, *wds)
+    wd_hash[pos].each do |wd|
+      matches << wd_matches_to_ph(ph_ary, wds + [wd])
+      next if pos != :second || (wds.join + wd).length > MAX_WD_LEN
 
-            if three_wd.length == 10 && wds_satisfy_pos?(0, first_wd + second_wd + trd_wd, ph_numbers)
-              full_match << "#{first_wd}, #{second_wd}, #{trd_wd}".downcase
-            end
-          end
-        end
+      matches = match_with_nxt_wd(:third, wd_hash, ph_ary, matches, wds + [wd])
+    end
+    matches.compact
+  end
+
+  # If the provided word to match with full phone number,
+  # it should match with phone number mapped character and have length 10
+  def self.wd_matches_to_ph(ph_ary, *wrds)
+    return unless wrds.join.length == PH_LENGTH && wds_satisfy_pos?(0, wrds.join, ph_ary)
+
+    wrds.join(', ').downcase
+  end
+
+  # Find 10 character words that matches the given phone number
+  # files_ary: Keeping track of already searched files
+  def self.find_ten_chr_wds(fst_wd, ph_ary, files_ary, matcd_ary)
+    filename = ten_chr_wd_filename(fst_wd)
+    new_file_path = find_file_to_search(3, filename)
+
+    if File.file?(new_file_path) && !files_ary.include?(filename)
+      File.open(new_file_path, 'r') do |f|
+        f.each_line { |line| (matcd_ary << line.strip.downcase) && break if wds_satisfy_pos?(0, line, ph_ary) }
       end
 
-      # Find 10 character words that matches the given phone number
-      filename = first_wd[0..2] + FILE_EXT
-      new_file_path = find_file_to_search(3, filename)
-      if File.file?(new_file_path) && !ten_char_seached_files.include?(filename)
-        File.open(new_file_path, 'r') do |f|
-          f.each_line { |line| (full_match << line.strip.downcase ) && break if wds_satisfy_pos?(0, line, ph_numbers) }
-        end
-        ten_char_seached_files << filename
-      end
+      files_ary << filename
     end
 
-    full_match
+    [matcd_ary, files_ary]
   end
 
   # @param first_pos [Integer] second_wd [String]
@@ -142,23 +146,17 @@ module PhNoToWord
   # and don't need to search at positions 7, 8 as even if the word is present
   # we cannot make second word to fulfil matching full ph no to words
   def self.search_it!(wrd, pos, word_no, ph_chars)
-    return unless wd_satisfy_to_search?(wrd, pos)
+    return unless valid_word?(wrd, pos)
 
     result = search_for_wd_match(wrd)
     return unless result
 
     already_scanned = already_scanned_for_nxt_wd?(word_no, result)
     add_result_to_store(result, word_no)
-    return unless result.length <= MAX_WD_LEN
+    return unless can_accomdte_another_wd?(result)
 
     nxt_wd_ary = ph_chars[result.length..-1]
     search_wd_in_nxt_pos(word_no, already_scanned, nxt_wd_ary)
-  end
-
-  # For word to search it should have minimum word length
-  # and length of the words are not in forbidden list
-  def self.wd_satisfy_to_search?(wrd, pos)
-    wrd.length >= MIN_WD_LENGTH && !FORBN_WD_LENS.include?(pos)
   end
 
   # Checks there is a need for search next word. If yes search for it
@@ -166,12 +164,6 @@ module PhNoToWord
     return unless first_or_sec_wd?(wd_no) && search_for_nxt_wd?(scanned, wd_ary.length)
 
     search_for_wds(wd_ary, 0, '', wd_no + 1)
-  end
-
-  # if not scanned earlier for next word and word must have min word length
-  # then gives permission for searching the next word
-  def self.search_for_nxt_wd?(scanned, wd_ary_len)
-    !scanned && wd_ary_len >= MIN_WD_LENGTH
   end
 
   # Adds the matching word to result store and position store
@@ -182,6 +174,9 @@ module PhNoToWord
     @matching_wd_hash[key_frm_word_no(word_no)] << result
   end
 
+  # Checks already scanned for next word with same positions by checking
+  # previous result length, for the results with same length don't need to
+  # search again for word from next positions 
   def self.already_scanned_for_nxt_wd?(word_no, result)
     return unless first_or_sec_wd?(word_no)
 
@@ -189,39 +184,12 @@ module PhNoToWord
     @matching_wd_hash[key].map(&:size).include?(result.length)
   end
 
-  # Scan the dictionary words with next character from the mapped ph no
+  # Scan the dictionary words with next character from the mapped phone number
   def self.scan_with_nxt_char(word_no, ph_chars, pos, wd_to_search)
     return unless search_again_for_first_wd?(word_no, pos) ||
                   search_again_for_second_thrid_wd?(word_no, pos, ph_chars)
 
     search_for_wds(ph_chars, pos + 1, wd_to_search, word_no)
-  end
-
-  # Search for first word until position 6
-  # Words with 7, 8 first wds will not counts
-  # Checks for 10 character words are done at last separately
-  def self.search_again_for_first_wd?(word_no, pos)
-    first_wd?(word_no) && pos <= MAX_WD_LEN_INDEX
-  end
-
-  # Search for second and thrid word until all remaining positions
-  def self.search_again_for_second_thrid_wd?(word_no, pos, ph_chars)
-    second_wd?(word_no) || third_wd?(word_no) && inside_last_pos?(ph_chars, pos)
-  end
-
-  # sets the variables that stores the data within loops
-  def self.set_data_stores
-    @result           ||= Set.new
-    @matching_wd_hash ||= { first: [], second: [], third: [] }
-    @scanned_pos      ||= []
-    nil
-  end
-
-  # reset the stored results data
-  def self.reset_data_stores
-    @result           = nil
-    @matching_wd_hash = nil
-    @scanned_pos      = nil
   end
 
   # finds the word with first, second and third character
@@ -233,23 +201,6 @@ module PhNoToWord
     return nil unless File.file?(new_file_path)
 
     check_file_cnt_matches(new_file_path, str)
-  end
-
-  # Level 1: 3 char words
-  # Level 2: > 3 char words
-  def self.filename_frm_lvl(str)
-    case str.length
-    when MIN_WD_LENGTH
-      level    = 1
-      filename = nil
-    when PH_LENGTH
-      level    = 3
-      filename = str[0..2] + FILE_EXT
-    else
-      level    = 2
-      filename = str[0..3] + FILE_EXT
-    end
-    [level, filename]
   end
 
   # if level is 1, search in file three_char_wrds
@@ -264,16 +215,17 @@ module PhNoToWord
   # @param file_path [String] str [String]
   # Ex: file_path /path/to/file/act.txt, str: acr
   def self.check_file_cnt_matches(file_path, str)
-    word_found = nil
+    wd_found = nil
     File.open(file_path, 'r') do |f|
-      f.each_line { |line| (word_found = line.strip) && break if cmp(line, str) }
+      f.each_line { |line| (wd_found = line.strip) && break if cmp(line, str) }
     end
-    word_found
+    wd_found
   end
 
   class << self
     private :cmp, :check_file_cnt_matches, :find_file_to_search,
             :search_for_wd_match, :reset_data_stores, :search_for_wds,
-            :wds_satisfy_pos?, :map_num_to_word_hash, :validate, :set_data_stores
+            :wds_satisfy_pos?, :map_num_to_word_hash, :validate,
+            :set_data_stores
   end
 end
