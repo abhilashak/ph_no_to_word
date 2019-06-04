711cc003f7e019b59518ef590aab7e6cdbc67b13
Abhilash A K <abhilashamballur@gmail.com>
2019-06-05 01:12:54 +0530

    Validator and other helper methods added

--- lib/ph_no_to_word/helpers.rb
+++ lib/ph_no_to_word/helpers.rb
@@ -6,6 +6,12 @@ module PhNoToWord
   module Helpers
     # Module methods
     module ClassMethods
+      include Constants
+
+      def self.extended(base)
+        base.send(:include, Constants)
+      end
+
       def first_wd?(word_no)
         word_no.zero?
       end
@@ -22,6 +28,67 @@ module PhNoToWord
         first_wd?(word_no) || second_wd?(word_no)
       end
 
+      # Compare two strings irrespective of case
+      def cmp(line, str)
+        return false unless line && str
+
+        line.strip.casecmp(str.strip).zero?
+      end
+
+      # Checks the position is within the given array
+      def inside_last_pos?(ary, pos)
+        pos < ary.length
+      end
+
+      # Provides the filename to search for 10 charcter word
+      def ten_chr_wd_filename(word)
+        word[0..2] + FILE_EXT
+      end
+
+      # For word to search it should have minimum word length
+      # and length of the words are not in forbidden list
+      def valid_word?(word, pos)
+        word.length >= MIN_WD_LENGTH && !FORBN_WD_LENS.include?(pos)
+      end
+
+      # Checks there is a room for another word within 10 character
+      def can_accomdte_another_wd?(result)
+        result.length <= MAX_WD_LEN
+      end
+
+      # Search for first word until position 6
+      # Words with 7, 8 first wds will not counts
+      # Checks for 10 character words are done at last separately
+      def search_again_for_first_wd?(word_no, pos)
+        first_wd?(word_no) && pos <= MAX_WD_LEN_INDEX
+      end
+
+      # Search for second and thrid word until all remaining positions
+      def search_again_for_second_thrid_wd?(word_no, pos, ary)
+        second_wd?(word_no) || third_wd?(word_no) && inside_last_pos?(ary, pos)
+      end
+
+      # if not scanned earlier for next word and word ary must have min word length
+      # for searching the word, then gives permission for searching the next word
+      def search_for_nxt_wd?(scanned, wd_ary_len)
+        !scanned && wd_ary_len >= MIN_WD_LENGTH
+      end
+
+      # sets the variables that stores the data within loops
+      def set_data_stores
+        @result           ||= Set.new
+        @matching_wd_hash ||= { first: [], second: [], third: [] }
+        @scanned_pos      ||= []
+        nil
+      end
+
+      # reset the stored results data
+      def reset_data_stores
+        @result           = nil
+        @matching_wd_hash = nil
+        @scanned_pos      = nil
+      end
+
       # provide the key according to first, second or third word
       def key_frm_word_no(word_no)
         case word_no
@@ -34,16 +101,22 @@ module PhNoToWord
         end
       end
 
-      # Compare two strings irrespective of case
-      def cmp(line, str)
-        return false unless line && str
-
-        line.strip.casecmp(str.strip).zero?
-      end
-
-      # Checks the position is within the given array
-      def inside_last_pos?(ary, pos)
-        pos < ary.length
+      # Level 1: 3 char words
+      # Level 2: > 3 char words
+      # Level 3: 10 char words
+      def filename_frm_lvl(str)
+        case str.length
+        when MIN_WD_LENGTH
+          level    = 1
+          filename = nil
+        when PH_LENGTH
+          level    = 3
+          filename = str[0..2] + FILE_EXT
+        else
+          level    = 2
+          filename = str[0..3] + FILE_EXT
+        end
+        [level, filename]
       end
     end
 
---
+++ lib/ph_no_to_word/validator.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'ph_no_to_word/error'
+
+# TODO: for future use
+module PhNoToWord
+  # helper module to expand functionalities
+  module Validator
+    # Module methods
+    module ClassMethods
+      include Constants
+      include Error
+
+      def self.extended(base)
+        base.send(:include, Constants)
+      end
+
+      # @param ph_no [String] ph_ary [Array]
+      # Populate error if not a valid phone number given
+      # Ex: ph_ary ["2", "2", "8", "2", "6", "6", "8", "6", "8", "7"]
+      def validate(ph_no, ph_ary)
+        raise RequiredArgumentMissingError, ERRORS[:missing_ph] if ph_no.empty?
+
+        unless ph_no.length.eql?(PH_LENGTH)
+          raise MalformattedArgumentError, ERRORS[:ph_length]
+        end
+        return if (ph_ary & FORBIDDEN_NOS).empty?
+
+        raise MalformattedArgumentError, ERRORS[:malformed_ph_no]
+      end
+    end
+
+    extend ClassMethods
+
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+  end
+end
